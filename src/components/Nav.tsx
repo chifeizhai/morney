6d35bc508126e782eb85b1cca1f0cfbfcab99c47
chifeizhai <465522282@qq.com>
2020-08-16 21:48:58 +0800

    '增加图标'

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 import React from 'react'
+require('icons/money.svg')
+require('icons/chart.svg')
+require('icons/tag.svg')
+
 const NavWrapper =styled.nav`
   line-height:24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -9,7 +13,10 @@ const NavWrapper =styled.nav`
     >li {
       width:33.3333%;
       text-align:center;
-      padding:16px;
+      padding:4px 0;
+      display:flex;
+      flex-direction:column;
+      align-items:center;
     }
   }
 `
@@ -18,12 +25,21 @@ const Nav = () => {
         <NavWrapper>
         <ul>
           <li>
+            <svg className="icon">
+                <use xlinkHref="#tag"/>
+            </svg>
             <Link to="/tags">标签页</Link>
           </li>
           <li>
+          <svg className="icon">
+                <use xlinkHref="#money"/>
+            </svg>
             <Link to="/money">记账页</Link>
           </li>
           <li>
+          <svg className="icon">
+                <use xlinkHref="#chart"/>
+            </svg>
             <Link to="/statistics">统计页</Link>
           </li>
         </ul>
