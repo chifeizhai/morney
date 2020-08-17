e1c306b12379367f515e1f7c9d1986f0600c41cb
chifeizhai <465522282@qq.com>
2020-08-18 00:57:35 +0800

    '再进一步'

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,6 +8,7 @@ require('icons/tag.svg')
 const NavWrapper = styled.nav`
   line-height:24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  background:#fff;
   > ul {
     display:flex;
     >li {
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -26,7 +26,22 @@ const TagsSection = styled.section`
   }
 `;
 const NotesSection = styled.section`
-
+  padding:0 16px;
+  font-size:14px;
+  > label{
+    display:flex;
+    align-items:center;
+    > span{
+      margin-right:16px;
+      white-space:nowrap;
+    }
+    > input {
+      width:100%;
+      height:72px;
+      background:none;
+      border:none;
+    }
+  }
 `
 const CategorySection = styled.section`
 
@@ -54,7 +69,7 @@ function Money() {
         <NotesSection>
           <label>
             <span>备注</span>
-            <input type="text"/>
+            <input type="text" placeholder="在这里添加备注"/>
           </label>
         </NotesSection>
 
