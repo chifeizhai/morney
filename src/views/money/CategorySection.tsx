668cb2bcb9c56c53a349b21ce744301135a00833
chifeizhai <465522282@qq.com>
2020-08-20 17:34:27 +0800

    '修复'

--- src/views/money/CategorySection.tsx
+++ src/views/money/CategorySection.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-
-const CategorySection = styled.section`
+import React, { useState } from 'react'
+const Wrapper = styled.section`
   font-size:24px;
   > ul{
     display:flex;
@@ -24,4 +24,22 @@ const CategorySection = styled.section`
   }
 `;
 
+const CategorySection: React.FunctionComponent = () => {
+  const categoryMap = { '-': '支出', '+': '收入' };
+  const [categoryList] = useState<('+' | '-')[]>(['-', '+']);
+  const [category, setCategory] = useState('-')
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c =>
+          <li key={c} 
+              className={category == c ? 'selected' : ''}
+              onClick={() => { setCategory(c); }}>
+            {categoryMap[c]}
+          </li>
+        )}
+      </ul>
+    </Wrapper>
+  );
+};
 export {CategorySection};
