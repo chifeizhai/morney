6974d9213af419a03c404453d263ca2f5f283a64
chifeizhai <465522282@qq.com>
2020-08-22 15:37:34 +0800

    '阿巴阿巴阿巴'

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,13 +1,13 @@
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 import { useTags } from 'components/useTags';
 import { useParams } from 'react-router-dom';
 import Layout from 'components/Layout';
-import {Button} from 'components/Botton';
+import { Button } from 'components/Botton';
 import styled from 'styled-components';
-import {Space} from 'components/Space';
+import { Space } from 'components/Space';
 require('icons/left.svg');
 
-const Topbar =styled.header`
+const Topbar = styled.header`
   display:flex;
   justify-content:space-between;
   align-items:center;
@@ -35,50 +35,57 @@ const Wrapper = styled.section`
     }
   }
 `;
-type Props={
-  value:string;
-  onChange:(value:string)=>void;
+type Props = {
+  value: any;
+  onChange: (value: any) => void;
 }
-const Laber:React.FunctionComponent<Props>= (props)=>{
-  const note=props.value
-  return(
+const Laber: React.FunctionComponent<Props> = (props) => {
+  const note = props.value
+  return (
     <Wrapper>
-    <label>
-      <span>备注</span>
-      <input type="text" placeholder="在这里添加备注"
-       value={note} onChange={(e)=>props.onChange(e.target.value)}
-      />
-    </label>
-  </Wrapper>
+      <label>
+        <span>标签名</span>
+        <input type="text" placeholder="在这里输入标签"
+          value={note} onChange={(e) => props.onChange(e)}
+        />
+      </label>
+    </Wrapper>
   )
 };
 
-const Tag:React.FunctionComponent=()=>{
-    const[selected,setSelected]=useState({note:''})
-    const {findTag}=useTags()
-    let{id}=useParams();
-    const tag=findTag(id)
-    return (
-        <Layout>
-            <Topbar>
-                <svg className="icon">
-                    <use xlinkHref="#left" />
-                </svg>
-                <span>编辑标签</span>
-                <svg className="icon">
-                    <use xlinkHref="#undefined"/>
-                </svg>
-            </Topbar>
-            <div>
-                <Laber value={selected.note}
-                     onChange={(note)=>{setSelected({...selected,note:note});
-                     }}/>
-            </div>
-            <Space>
-                <Button>删除标签</Button>
-            </Space>
-        </Layout>
-    );
+const Tag: React.FunctionComponent = () => {
+  const { findTag, updateTag,deleteTag } = useTags()
+  let { id } = useParams();
+  const tag = findTag(parseInt(id))
+  const tagContent=(tag:{id:number;name:string})=>(
+    <div>
+        <Laber value={tag.name}
+          onChange={(e) => {
+            updateTag(tag.id, { name: e.target.value });
+          }} />
+        <Space>
+          <Button onClick={() => deleteTag(tag.id)}>删除标签</Button>
+        </Space>
+      </div>
+  )
+  return (
+    <Layout>
+      <Topbar>
+        <svg className="icon">
+          <use xlinkHref="#left" />
+        </svg>
+        <span>编辑标签</span>
+        <svg className="icon">
+          <use xlinkHref="#undefined" />
+        </svg>
+      </Topbar>
+
+      {tag ? tagContent(tag): <div>tag 不存在</div>}
+
+
+    </Layout>
+  );
+
 };
 
-export{Tag};
+export { Tag };
